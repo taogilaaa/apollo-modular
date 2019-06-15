bf7a70b82e0a0b95abba946afa718fb8a61fba84
Alfian Pramudita <alfian.pramudita@gmail.com>
2019-06-15 10:11:47 +0700

    Implement new schema creation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import { ApolloServer, mergeSchemas } from 'apollo-server';
-import helloSchema from './hello';
-import peopleSchema from './people';
-import postSchema from './post';
+import { ApolloServer, makeExecutableSchema } from 'apollo-server';
+import hello from './hello';
+import people from './people';
+import post from './post';
+import baseTypeDefs from './schema.graphql';
 
-const schema = mergeSchemas({
-  schemas: [helloSchema, peopleSchema, postSchema],
+const schema = makeExecutableSchema({
+  typeDefs: [baseTypeDefs, hello.typeDefs, people.typeDefs, post.typeDefs],
+  resolvers: [hello.resolvers, people.resolvers, post.resolvers],
 });
 
 const server = new ApolloServer({
